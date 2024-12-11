b6df0e8009ff11eca53c205eb687c4c408000da3
halifox <106858061+halifox@users.noreply.github.com>
2024-12-11 13:23:52 +0800

    Update version name to "1.0.6"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.github.dictionary"
         minSdk = 21
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.5"
+        versionCode = 6
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
