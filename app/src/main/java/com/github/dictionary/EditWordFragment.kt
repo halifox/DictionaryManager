14051bd8b081b5a61d67db3ffd4b3cb8d355278b
halifox <106858061+halifox@users.noreply.github.com>
2024-12-21 00:35:04 +0800

    Add edit functionality and update UI for word management features.

--- app/src/main/java/com/github/dictionary/EditWordFragment.kt
+++ app/src/main/java/com/github/dictionary/EditWordFragment.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.github.dictionary.databinding.FragmentEditWordBinding
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import org.koin.android.ext.android.inject
 
@@ -40,81 +41,114 @@ class EditWordFragment : Fragment() {
         _binding = null
     }
 
+    private val _type by lazy { requireArguments().getInt(TYPE) }
+    private val _word by lazy { requireArguments().getString(WORD) }
+    private val _frequency by lazy { requireArguments().getInt(FREQUENCY, 250) }
+    private val _locale by lazy { requireArguments().getString(LOCALE) }
+    private val _appid by lazy { requireArguments().getInt(APPID, 0) }
+    private val _shortcut by lazy { requireArguments().getString(SHORTCUT) }
+
 
     private val userDictionaryManager by inject<UserDictionaryManager>()
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val _type = requireArguments().getInt(TYPE)
-        val _word = requireArguments().getString(WORD)
-        val _frequency = requireArguments().getInt(FREQUENCY, 250)
-        val _locale = requireArguments().getString(LOCALE)
-        val _appid = requireArguments().getInt(APPID, 0)
-        val _shortcut = requireArguments().getString(SHORTCUT)
+        fillText()
+        setupToolbar()
+    }
 
+    private fun fillText() {
         binding.word.setText(_word)
         binding.frequency.setText(_frequency.toString())
         binding.locale.setText(_locale)
         binding.appid.setText(_appid.toString())
         binding.shortcut.setText(_shortcut)
+    }
 
-        binding.delete.isVisible = _type == TYPE_UPDATE
-        binding.delete.setOnClickListener {
-            userDictionaryManager.delete(word = _word, shortcut = _shortcut, frequency = _frequency, locale = _locale, appid = _appid)
-            findNavController().navigateUp()
+
+    private fun setupToolbar() {
+        val toolbar = requireActivity().findViewById<MaterialToolbar>(R.id.toolbar)
+        toolbar.inflateMenu(R.menu.menu_edit_word)
+        val add = toolbar.menu.findItem(R.id.add)
+        val update = toolbar.menu.findItem(R.id.update)
+        val delete = toolbar.menu.findItem(R.id.delete)
+        add.isVisible = _type == TYPE_ADD
+        update.isVisible = _type == TYPE_UPDATE
+        delete.isVisible = _type == TYPE_UPDATE
+        toolbar.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.add -> add()
+                R.id.update -> update()
+                R.id.delete -> delete()
+            }
+            true
+        }
+    }
+
+
+    private fun check(callback: (word: String, shortcut: String?, frequency: Int?, locale: String?, appid: Int?) -> Unit) {
+        val word = binding.word.text?.toString()
+        val shortcut = binding.shortcut.text?.toString()
+        val frequency = binding.frequency.text?.toString()?.toIntOrNull()
+        var locale = binding.locale.text?.toString()
+        val appid = binding.appid.text?.toString()?.toIntOrNull()
+
+        if (locale.isNullOrEmpty()) {
+            locale = null // 处理local = ""的情况
         }
 
-        binding.save.setOnClickListener {
-            val word = binding.word.text?.toString()
-            val shortcut = binding.shortcut.text?.toString()
-            val frequency = binding.frequency.text?.toString()?.toIntOrNull()
-            var locale = binding.locale.text?.toString()
-            val appid = binding.appid.text?.toString()?.toIntOrNull()
-
-            if (locale.isNullOrEmpty()) {
-                locale = null // 处理local = ""的情况
-            }
-
-            if (word.isNullOrBlank()) {
-                MaterialAlertDialogBuilder(requireContext())
-                    .setTitle(R.string.dialog_add_error_titile)
-                    .setMessage(R.string.dialog_add_word_error_message)
-                    .setPositiveButton(R.string.dialog_yes) { _, _ -> }
-                    .show()
-                return@setOnClickListener
-            }
-            if (frequency == null || frequency < 1 || frequency > 255) {
-                MaterialAlertDialogBuilder(requireContext())
-                    .setTitle(R.string.dialog_add_error_titile)
-                    .setMessage(R.string.dialog_add_frequency_error_message)
-                    .setPositiveButton(R.string.dialog_yes) { _, _ -> }
-                    .show()
-                return@setOnClickListener
-            }
-            when (_type) {
-                TYPE_ADD -> userDictionaryManager.insert(
-                    word = word,
-                    shortcut = shortcut,
-                    frequency = frequency,
-                    locale = locale,
-                    appid = appid,
-                )
-
-                TYPE_UPDATE -> userDictionaryManager.update(
-                    _word = _word,
-                    _shortcut = _shortcut,
-                    _frequency = _frequency,
-                    _locale = _locale,
-                    _appid = _appid,
-                    word = word,
-                    shortcut = shortcut,
-                    frequency = frequency,
-                    locale = locale,
-                    appid = appid,
-                )
-            }
-            findNavController().navigateUp()
+        if (word.isNullOrBlank()) {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(R.string.dialog_add_error_titile)
+                .setMessage(R.string.dialog_add_word_error_message)
+                .setPositiveButton(R.string.dialog_yes) { _, _ -> }
+                .show()
+            return
         }
+        if (frequency == null || frequency < 1 || frequency > 255) {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(R.string.dialog_add_error_titile)
+                .setMessage(R.string.dialog_add_frequency_error_message)
+                .setPositiveButton(R.string.dialog_yes) { _, _ -> }
+                .show()
+            return
+        }
+
+        callback(word, shortcut, frequency, locale, appid)
+    }
+
+    private fun add() {
+        check { word, shortcut, frequency, locale, appid ->
+            userDictionaryManager.insert(
+                word = word,
+                shortcut = shortcut,
+                frequency = frequency,
+                locale = locale,
+                appid = appid,
+            )
+        }
+    }
+
+    private fun update() {
+        check { word, shortcut, frequency, locale, appid ->
+            userDictionaryManager.update(
+                _word = _word,
+                _shortcut = _shortcut,
+                _frequency = _frequency,
+                _locale = _locale,
+                _appid = _appid,
+                word = word,
+                shortcut = shortcut,
+                frequency = frequency,
+                locale = locale,
+                appid = appid,
+            )
+        }
+    }
+
+    private fun delete() {
+        userDictionaryManager.delete(word = _word, shortcut = _shortcut, frequency = _frequency, locale = _locale, appid = _appid)
+        findNavController().navigateUp()
     }
 }
