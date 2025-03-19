647f61a7bf8747c2037fc8c85f31bf06db7a7975
halifox <106858061+halifox@users.noreply.github.com>
2025-03-20 00:41:40 +0800

    Add navigation icons and search functionality to the dictionary screen.

--- app/src/main/java/com/github/dictionary2/ui/DictionaryIndexScreen.kt
+++ app/src/main/java/com/github/dictionary2/ui/DictionaryIndexScreen.kt
@@ -6,14 +6,26 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -29,23 +41,39 @@ import org.koin.compose.koinInject
 @Preview
 @Composable
 fun DictionaryIndexScreen() {
+    val navHostController = LocalNavController.current
+    val dao: SogouDao = koinInject()
+    var key by remember { mutableStateOf("") }
+    val pager = remember(key) {
+        Pager(
+            config = PagingConfig(pageSize = 20),
+            pagingSourceFactory = { dao.pagingSource("%${key}%") }
+        )
+    }
+    val lazyPagingItems = pager.flow.collectAsLazyPagingItems()
     Scaffold(
         topBar = {
             TopAppBar(
                 title = {
                     Text("词典")
-                }
+                },
+                navigationIcon = {
+                    IconButton({
+                        navHostController.navigateUp()
+                    }) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, null)
+                    }
+                },
+                actions = {
+                    IconButton({
+
+                    }) {
+                        Icon(Icons.Default.Search, null)
+                    }
+                },
             )
         }
     ) {
-        val dao: SogouDao = koinInject()
-        val pager = remember {
-            Pager(
-                config = PagingConfig(pageSize = 20),
-                pagingSourceFactory = { dao.pagingSource("%%") }
-            )
-        }
-        val lazyPagingItems = pager.flow.collectAsLazyPagingItems()
         LazyColumn(
             modifier = Modifier.padding(it)
         ) {
