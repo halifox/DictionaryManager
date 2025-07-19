ed7adf21acfbf28291b12884e1227a2de5c41fd4
halifox <106858061+halifox@users.noreply.github.com>
2025-07-19 11:49:35 +0800

    - Update `Dict` entity to allow nullable fields - Enable Room schema export for `AppDatabase`

--- app/src/main/java/com/github/dictionary/db/AppDatabase.kt
+++ app/src/main/java/com/github/dictionary/db/AppDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.github.dictionary.model.Dict
 
-@Database(entities = [Dict::class], version = 1, exportSchema = false)
+@Database(entities = [Dict::class], version = 1, exportSchema = true)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun dictDao(): DictDao
 }
--- app/src/main/java/com/github/dictionary/model/Dict.kt
+++ app/src/main/java/com/github/dictionary/model/Dict.kt
@@ -6,13 +6,13 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "dict")
 data class Dict(
     @PrimaryKey(autoGenerate = true) val _id: Int,
-    val source: String,
-    val id: String,
-    val pid: String,
-    val name: String,
-    val innerId: String,
-    val time: String,
-    val downCount: String,
-    val exps: String,
-    val tiers: Int,
+    val source: String?,
+    val id: String?,
+    val pid: String?,
+    val name: String?,
+    val innerId: String?,
+    val time: String?,
+    val downCount: String?,
+    val exps: String?,
+    val tiers: String?,
 )
