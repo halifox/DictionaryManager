73ed8c05492a3bf937ee23d6eebbec6bb5f94161
halifox <106858061+halifox@users.noreply.github.com>
2025-07-28 02:09:37 +0800

    Update app version to 2.0.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.github.dictionary"
         minSdk = 21
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 200
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
